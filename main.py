089bd62c12142837b71d4158008db062c54bcffc
Sviatoslav Ishchenko <isv.emm@gmail.com>
2026-08-19 15:49:05 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# commit - збереження змін у коді, хто зробив зміни, що змінено і т.д.
+# pull request - запит на оновлення даних
+# червоний файл гіт не бачить
+# зелений - доданий до гіта, але не закомічений
+# блакитний - файл доданий до гіта, і в якому були якісь зміни
+# сірий - поточний файл без змін, гіт бачить його його
+# git init - ініціалізація гіта, щоб гіт слідкував за проектом
+# git status - інформація поточного стану проекту
+# git remote -v - чи підв'язані до Гітхабу
+# git branch -M main - пеерейменування головної гілки
+# git add main.py - додавання файлу до гіта
+# git commit -m "Initial commit" - коміт файлу (зміна) і коментар
+# clear - очистити консоль
+# git log - що відбувалося в проекті
+# git add . - додати всі файли до гіта
+# репозиторій - проект, за яким слідкує гіт
+# origin - посилання на репозиторій https://github.com/SviatoslavIshchenko/git-practice.git
+# git remote add origin https://github.com/SviatoslavIshchenko/git-practice.git
+# git-learning> git remote -v - підключення до сервера і отримання інформації
+# git push -u origin main - відправлення оновлених файлів на Гітхаб при першому відправленні
+# git push -u origin new - відправлення нової гілки на Гітхаб
+# git pull - відправлення коду на локальний компютер
+# git push - відправлення оновлених файлів на Гітхаб
+# git diff - показати різницю між файлами в більшості на сервері
+# git branch feacher-login - Створення нової гілки
+# git switch feacher-login - переключення на гілку
+# git-practice> git push origin --delete feacher-login - видалення гілки
+
+print("Hello Git!!!")
+
+print("Hello Git!!!")
+
+print("Hello Git!!!  111")
+
+print("Hello Git!!!  111")
